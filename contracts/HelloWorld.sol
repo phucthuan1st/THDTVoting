4b7d3d63efc6a18aa8eaaee382787c4e457e0e92
phucthuan1st <phucthuan1st@gmail.com>
2023-12-25 17:22:19 +0700

    first create repository

---
+++ contracts/HelloWorld.sol
@@ -0,0 +1,15 @@
+// HelloWorld.sol
+pragma solidity ^0.8.0;
+
+contract HelloWorld {
+    string public greeting;
+
+    constructor() {
+        greeting = "Hello, World!";
+    }
+
+    function getGreeting() public view returns (string memory) {
+        return greeting;
+    }
+}
+
